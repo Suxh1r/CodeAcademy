060aa030dce06b3c090df3c7f909da84aa079900
Suxh1r <tumulurusuchir@gmail.com>
2020-01-03 16:24:01 -0600

    Update to Git file 3

--- Git lang.js
+++ Git lang.js
@@ -1,6 +1,7 @@
 //type bash into terminal
 //ls 
 //git commit
+//git add if you make a new file or folder
 //git pull to pull a file from the cloud
 //git push to push a file into the cloud
 //This saves through the program
