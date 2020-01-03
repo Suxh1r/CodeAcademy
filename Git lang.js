80ae7dcef581826766d817ba178025885c2732aa
Suxh1r <such1r@users.noreply.github.com>
2020-01-02 22:34:41 -0600

    Update Git lang.js
    Donzo

--- Git lang.js
+++ Git lang.js
@@ -1,5 +1,6 @@
+//type bash into terminal
 //ls 
 //git commit
-//git pull
-//git push
+//git pull to pull a file from the cloud
+//git push to push a file into the cloud
 //This saves through the program
